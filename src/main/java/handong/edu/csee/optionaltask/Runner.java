fb48a6ae67503ff1dc6a10b83a15bdae72de75ef
Kimminju98 <21700097@handong.edu>
2018-06-20 14:01:29 +0900

    correct runner class

--- src/main/java/handong/edu/csee/optionaltask/Runner.java
+++ src/main/java/handong/edu/csee/optionaltask/Runner.java
@@ -10,85 +10,92 @@ import org.apache.commons.cli.Options;
 
 
 public class Runner {
-	
+
 	String urlPath;
 	String outputPath;
 	boolean help;
-	
-	
+
+
 	public static void main(String[] args) {
 		Runner myRunner = new Runner();
 		myRunner.run(args);
 	}
-	
+
 	private void run(String[] args) {
 		Options options = createOptions();
-		
+
 		if(parseOptions(options, args)){
 			if (help){
 				printHelp(options);
 				return;
 			}
-			
+
 			URLReader urlreader = new URLReader(urlPath);
-			String result = urlreader.getResult();
+			String result="";
+			try {
+				result = urlreader.getResult();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			DataWriter urlwriter = new DataWriter(outputPath,result);
-			
+			urlwriter.getOutputFile();
+
 		}
 	}
-	
-	
+
+
 	private boolean parseOptions(Options options,String[] args) {
 		CommandLineParser parser = new DefaultParser();
-		
+
 		try {
 			CommandLine cmd = parser.parse(options, args);
-			
+
 			urlPath = cmd.getOptionValue("u");
 			outputPath = cmd.getOptionValue("d");
 			help = cmd.hasOption("h");
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			printHelp(options);
 			return false;
 		}
-		
+
 		return true;
-		
+
 	}
-	
-	
+
+
 	private Options createOptions() {
 		Options options = new Options();
-		
+
 		options.addOption(Option.builder("u").longOpt("urlPath")
 				.desc("Set a urlPath")
 				.hasArg()
 				.argName("URL Path")
 				.required()
 				.build());
-		
+
 		options.addOption(Option.builder("d").longOpt("ouputdir")
 				.desc("Set a filename")
 				.hasArg()
 				.argName("filename to display")
 				.required()
 				.build());
-		
+
 		options.addOption(Option.builder("h").longOpt("help")
 				.desc("Help")
 				.build());
-		
+
 		return options;
-		
+
 	}
-	
+
 	private void printHelp(Options options) {
 		// automatically generate the help statement
 		HelpFormatter formatter = new HelpFormatter();
 		String header = "WebPageCrawler program";
-		String footer ="\nPlease report issues to 21700097@handong.edu (emails)"; //옵션 설명 후 등장!
+		String footer ="\nPlease report issues to 21700097@handong.edu (emails)"; 
 		formatter.printHelp("CLIExample", header, options, footer, true);
 	}
 
--- src/main/java/handong/edu/csee/optionaltask/URLReader.java
+++ src/main/java/handong/edu/csee/optionaltask/URLReader.java
@@ -2,43 +2,34 @@ package handong.edu.csee.optionaltask;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
 public class URLReader {
-	
+
 	String urlPath;
 	StringBuilder contents = new StringBuilder();
 	String result="";
-	
+
 	public URLReader(String urlPath) {
 		this.urlPath = urlPath;
 	}
-	
-	public String getResult() {
-		
-		try {
-			URL url = new URL(urlPath);
-			URLConnection connection = (URLConnection)url.openConnection();
-			InputStreamReader reader = new InputStreamReader(connection.getInputStream(),"utf-8");
-			BufferedReader buff = new BufferedReader(reader);
-			
-			while((result = buff.readLine())!=null) {
-				contents.append(result);
-				contents.append("\r\n");
-			}
-			buff.close();
-			    System.out.println(contents.toString());
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
+
+	public String getResult() throws Exception{
+
+
+		URL oracle = new URL(urlPath);
+		BufferedReader in = new BufferedReader(new InputStreamReader(oracle.openStream()));
+
+		String inputLine;
+		while ((inputLine = in.readLine()) != null)
+			//System.out.println(inputLine);
+			result=result+inputLine;
+		in.close();
+
+
 		return result;
 	}
-	
+
 
 }
